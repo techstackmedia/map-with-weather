00c904b45b5bc6cb5e5dbd75a5a3b657b303dd0d
Osagie Bello <osagienoah04@gmail.com>
2025-06-17 23:19:16 +0100

    feat: show all countries at initial search state

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,14 +1,16 @@
-import { cities } from '@/utiles/cities';
-import { useState } from 'react';
+import { cities } from "@/utiles/cities";
+import { useState } from "react";
 
 type SidebarProps = {
   onSelect: (lat: number, lon: number, name: string) => void;
 };
 
 export default function Sidebar({ onSelect }: SidebarProps) {
-  const [search, setSearch] = useState('');
+  const [search, setSearch] = useState("");
 
-  const filtered = cities.filter(city => city.name.toLowerCase().includes(search.toLowerCase()));
+  const filtered = cities.filter((city) =>
+    city.name.toLowerCase().includes(search.toLowerCase())
+  );
 
   return (
     <div className="p-4 w-full md:w-64 bg-white max-h-[50vh] md:max-h-full overflow-y-auto border-b md:border-b-0 md:border-r border-gray-200">
@@ -17,11 +19,10 @@ export default function Sidebar({ onSelect }: SidebarProps) {
         placeholder="Search city..."
         className="w-full mb-4 p-2 border rounded"
         value={search}
-        onChange={e => setSearch(e.target.value)}
+        onChange={(e) => setSearch(e.target.value)}
       />
-      {
-        search.length === 0 ? null : <ul className="space-y-1">
-        {filtered.map(city => (
+      <ul className="space-y-1">
+        {filtered.map((city) => (
           <li
             key={city.name}
             className="cursor-pointer hover:underline"
@@ -31,8 +32,6 @@ export default function Sidebar({ onSelect }: SidebarProps) {
           </li>
         ))}
       </ul>
-      }
-      
     </div>
   );
 }
